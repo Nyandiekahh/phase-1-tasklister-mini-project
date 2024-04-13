dbacd2d64c6b120e0c1582c65972b9a5d30bd7d4
Nyandieka <einsteinmokua100@gmail.com>
2024-04-13 16:50:31 +0300

    Creates a simple tasklister-mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+    const form = document.getElementById('create-task-form');
+    const taskList = document.getElementById('tasks');
+  
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const taskInput = document.getElementById('new-task-description');
+      const taskDescription = taskInput.value.trim();
+  
+      if (taskDescription !== '') {
+        addTask(taskDescription);
+        taskInput.value = '';
+      }
+    });
+  
+    function addTask(description) {
+      const taskItem = createTaskItem(description);
+      taskList.appendChild(taskItem);
+    }
+  
+    function createTaskItem(description) {
+      const li = document.createElement('li');
+      li.textContent = description;
+      return li;
+    }
+  });
